7ed01f541af68a89f28d15454fcf1a8798e82348
mashfikur <120697837+mashfikur@users.noreply.github.com>
2023-09-23 23:00:08 +0600

    view and hide jobs functionality added

--- src/assets/components/FeaturedJobs.jsx
+++ src/assets/components/FeaturedJobs.jsx
@@ -3,14 +3,16 @@ import SectionTitle from "./SectionTitle";
 import Job from "./Job";
 
 const FeaturedJobs = () => {
-    const [jobs,setJobs]=useState([])
-    useEffect(()=>{
-        fetch('jobs.json')
-        .then(res=>res.json())
-        .then(data=>setJobs(data))
-    },[])
+  const [jobs, setJobs] = useState([]);
+  const [slicedList, setSlicedList] = useState(4);
+
+  useEffect(() => {
+    fetch("jobs.json")
+      .then((res) => res.json())
+      .then((data) => setJobs(data));
+  }, []);
   return (
-    <div className="max-w-[80rem] mx-auto">
+    <div className="max-w-[80rem] mx-auto mb-32 ">
       <SectionTitle title={"Featured Jobs"}>
         <p>
           Explore thousands of job opportunities with all the information you
@@ -18,10 +20,31 @@ const FeaturedJobs = () => {
         </p>
       </SectionTitle>
 
-      <div className="grid grid-cols-2">
-        {
-            jobs.map(job=><Job key={job.id} job={job} ></Job>)
-        }
+      <div className="grid grid-cols-2 gap-6 mb-10">
+        {jobs.slice(0, slicedList).map((job) => (
+          <Job key={job.id} job={job}></Job>
+        ))}
+      </div>
+
+      <div className="flex flex-col items-center">
+        <button
+          onClick={() => setSlicedList(jobs.length)}
+          className={`btn btn-own ${
+            slicedList=== jobs.length ? "hidden":""
+          } `}
+        >
+          See All Jobs
+        </button>
+
+
+        <button
+          onClick={() => setSlicedList(4)}
+          className={`btn btn-own ${
+            slicedList=== jobs.length ? "":"hidden"
+          } `}
+        >
+          See Less 
+        </button>
       </div>
     </div>
   );
--- src/assets/components/Job.jsx
+++ src/assets/components/Job.jsx
@@ -12,11 +12,12 @@ const Job = ({ job }) => {
     salary,
   } = job;
   return (
-    <div className="border-2 rounded p-14">
+    <div className="border-2  rounded p-14">
       <img src={logo} alt="" />
 
       <h3 className="text-xl mt-8 font-extrabold">{job_title}</h3>
       <h4 className="font-semibold mb-4 text-[#757575]">{company_name}</h4>
+
       <div>
         <div className="flex gap-4 ">
           <h3 className="px-4 py-2 border-2 rounded border-[#9873FF] text-[#9873FF]">
@@ -27,7 +28,7 @@ const Job = ({ job }) => {
           </h3>
         </div>
 
-        <div className="flex items-center gap-4 mt-4">
+        <div className="flex items-center gap-4 mt-4 mb-4">
           <h3 className="flex items-center font-semibold text-[#757575]">
             {" "}
             <CiLocationOn></CiLocationOn> {location}
@@ -36,6 +37,9 @@ const Job = ({ job }) => {
             {" "}
             <CiDollar></CiDollar> {salary}
           </h3>
+        </div>
+        <div>
+          <button className="btn btn-own" >View Details</button>
         </div>
       </div>
     </div>
